8cec7078c86d9805e51948a93ebdd6b442dfb6c4
anubhavmohandas <anubhav.manav147@gmail.com>
2022-02-15 19:38:06 +0530

    node

--- restapi/app.js
+++ restapi/app.js
@@ -2,7 +2,8 @@ let express = require('express');
 let app = express();
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
-const mongoUrl = "mongodb://localhost:27017"
+// const mongoUrl = "mongodb://localhost:27017"
+const mongoUrl = "mongodb+srv://test:test123@cluster0.p7mvq.mongodb.net/novintern?retryWrites=true&w=majority"
 const dotenv = require('dotenv');
 dotenv.config()
 let port = process.env.PORT || 8210;
@@ -13,21 +14,19 @@ app.get('/',(req,res) => {
     res.send("Welcome to express")
 })
 
-// /* Get City */
-// app.get('/city',(req,res) => {
-//     res.send(city)
-// })
-
-// /* Get hotel */
-// app.get('/hotel',(req,res) => {
-//     res.send(hotel)
-// })
 
 /* Get Location */
 app.get('/location',(req,res) => {
     db.collection('location').find().toArray((err, result) =>{
         if(err) throw err;
-        res.send(hotel)
+        res.send(result)
+    })
+})
+
+app.get('/restaurantmenu',(req,res) => {
+    db.collection('restaurantmenu').find().toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result)
     })
 })
 
